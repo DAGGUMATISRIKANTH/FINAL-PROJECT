f576a68bd903145eea3755a38102ec7624b4d655
DAGGUMATISRIKANTH <60091725+DAGGUMATISRIKANTH@users.noreply.github.com>
2020-03-27 19:12:27 +0530

    Update Excel.java

--- src/main/java/ExcelUtility/Excel.java
+++ src/main/java/ExcelUtility/Excel.java
@@ -15,9 +15,9 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 		public  String excel_username(int a) throws IOException {
 			  
 			  FileInputStream fil = new FileInputStream(new File("C:\\Users\\SRIKANTH\\eclipse-workspace\\DEMOBLAZE-R\\src\\test\\resources\\Testdata\\ExcelInputs.xlsx"));
-			  XSSFWorkbook workbook = new XSSFWorkbook(fil);
-			  XSSFSheet sheet=workbook.getSheet("Sheet1");
-				  String un=sheet.getRow(a).getCell(0).getStringCellValue();
+			  XSSFWorkbook workbook = new XSSFWorkbook(fil);//Reading workbook form file
+			  XSSFSheet sheet=workbook.getSheet("Sheet1");//reading sheet from workbook
+				  String un=sheet.getRow(a).getCell(0).getStringCellValue();//Reading username from sheet1
 				  
 	          
 			return un;
@@ -25,9 +25,9 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 		public  String excel_password(int b) throws IOException {
 			  
 			  FileInputStream fil = new FileInputStream(new File("C:\\Users\\SRIKANTH\\eclipse-workspace\\DEMOBLAZE-R\\src\\\\test\\resources\\Testdata\\ExcelInputs.xlsx"));
-			  XSSFWorkbook workbook = new XSSFWorkbook(fil);
-			  XSSFSheet sheet=workbook.getSheet("Sheet1");
-			  String pwd=sheet.getRow(b).getCell(1).getStringCellValue();
+			  XSSFWorkbook workbook = new XSSFWorkbook(fil);//Reading workbook form file
+			  XSSFSheet sheet=workbook.getSheet("Sheet1");//reading sheet from workbook
+			  String pwd=sheet.getRow(b).getCell(1).getStringCellValue();//reading password from sheet1
 			  
 	return pwd;
 	}
